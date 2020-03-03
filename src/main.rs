0551825e04159c3ec24112c17c5bbf8f4fbbb65d
Eric Pederson <ericacm+github@gmail.com>
2020-03-02 22:14:22 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+use minigrep::run;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args)
+        .unwrap_or_else(|e| {
+            eprintln!("Problem parsing arguments: {}", e);
+            process::exit(1);
+        });
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
